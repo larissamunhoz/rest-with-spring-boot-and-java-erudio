5d3609072464ecb83eb564d0be81b9f32ab3a170
larissamunhoz <larissa.munhoz.03@gmail.com>
2023-01-29 18:12:04 -0300

    cswagger configuration and customization

---
+++ src/main/java/br/com/erudio/config/OpanApiConfig.java
@@ -0,0 +1,25 @@
+package br.com.erudio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpanApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API with Java 18 and Spring Boot 3")
+                        .version("v1")
+                        .description("Some description about your API")
+                        .termsOfService("")
+                        .license(new License().name("Apache 2.0")
+                                .url("")));
+    }
+
+    //http://localhost:8080/swagger-ui/index.html
+}
